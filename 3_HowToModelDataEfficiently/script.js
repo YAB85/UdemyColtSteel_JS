8351b80b2965684c26d054c18dffc55d95d16de1
Yaroslav <boyaal24@gmail.com>
2020-04-30 20:32:03 +0300

    strings and values

---
+++ 3_HowToModelDataEfficiently/script.js
@@ -0,0 +1,49 @@
+/* let isLoggedIn = true;
+
+let gameOver = false;
+
+const isWaterWet = true; */
+
+/*****Strings */
+
+/* let text = 'text';
+let text1 = "text";
+let text2 = 'text sadsa " " dsad';
+let text3 = '87';
+let text4 = "45"; */
+
+/*******Strings are indexed*/
+/* 
+let mySong = "dsfdsfs dfsf";
+
+
+mySong.toUpperCase();
+
+console.log(mySong); */
+
+/* let text = "     dfsd    fsdf";
+
+let x = text.trim().toUpperCase();
+
+
+
+console.log(x); */
+
+/***** indexOf */
+
+
+'Baseball'.indexOf("ball"); // 4
+'Baseball'.indexOf("B"); // 0
+
+/********Slice */
+
+"Baseball".slice(4); //"ball"
+
+"superhero".slice(0,5);// "super"
+
+
+
+
+
+
+
