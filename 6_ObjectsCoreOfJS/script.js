f835b79c33856bc4e7ca41b56ca0c6ea212d24b1
Yaroslav <boyaal24@gmail.com>
2020-05-06 21:47:17 +0300

    add task -> object

---
+++ 6_ObjectsCoreOfJS/script.js
@@ -0,0 +1,110 @@
+/******Create object */
+const fitBitData = {
+  totalSteps: 308727,
+  totalMiles: 211.7,
+  avgCalorieBurn: 5755,
+  workoutsThisWeek: '5 of 7',
+  avgGoodSleep: 308727,
+};
+
+//fitBitData.totalMiles = 12231;
+
+//console.log(fitBitData);
+/******Accessing Object Property */
+
+/* const palette = {
+  red : '#eb4d4b',
+  yellow : '#f9ca24',
+  blue : '#30336b',
+  10: 'ten',
+  1000: 'one thousend'
+}
+
+let mysteryColor = 'yellow';
+
+console.log(palette[mysteryColor]);
+
+/*****Adding and Updating Properties */
+ 
+/* const userReviews = {};
+
+userReviews['queenBee49'] = 4.0;
+userReviews.queenBee33 = 3.5;
+
+
+userReviews['queenBee49'] += 3.5;
+userReviews.queenBee33++;
+
+
+console.log(userReviews); */
+
+/******Nested Array & Objects */
+
+/* const student = {
+  firstName: 'David',
+  lastName : 'Jones',
+  strengths : ['Music', 'Art'],
+  exams : {
+    midterm: 92,
+    final: 88
+  }
+};
+
+const shoppingCart = [
+    {
+      product: 'Jenga Classic',
+      price: 6.88,
+      quantity: 1
+    },
+    {
+      product: 'Ps4 Classic',
+      price: 4.23,
+      quantity: 2
+    },
+    {
+      product: 'Sony Classic',
+      price: 9.99,
+      quantity: 5
+    },
+    {
+      product: 'Sega Classic',
+      price: 5.66,
+      quantity: 4
+    },
+
+];
+
+console.log(student); */
+
+/* const student = {
+  firstName: 'David',
+  lastName : 'Jones',
+  strengths : ['Music', 'Art'],
+  exams : {
+    midterm: 92,
+    final: 88
+  }
+};
+
+const avg = (student.exams.midterm + student.exams.final) / 2; */
+
+//console.log(avg);
+
+const palette = {
+  red : '#eb4d4b',
+  yellow : '#f9ca24',
+  blue : '#30336b',
+};
+
+const palette2 = palette;
+
+palette2.green = '#eb4b2a';
+
+console.log(palette);
+console.log(palette2);
+
+
+
+
+
+
