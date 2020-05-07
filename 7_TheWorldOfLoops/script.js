522f49e18975f9a416fd9f1111f61be17d18d365
Yaroslav <boyaal24@gmail.com>
2020-05-08 00:06:04 +0300

    add task loops -> nested for loops

--- 7_TheWorldOfLoops/script.js
+++ 7_TheWorldOfLoops/script.js
@@ -32,7 +32,7 @@ for (let i = 0; i < examScores.length; i++ ) {
   console.log(i, examScores[i]);
 } */
 
-const myStudents = [
+/* const myStudents = [
   {
     firstName : 'Zeus',
     grade     : 86
@@ -68,4 +68,29 @@ for(let i = word.length - 1; i >= 0; i--) {
   reversedWord += word[i];
 }
 
-console.log(reversedWord);
+console.log(reversedWord); */
+
+/****Nested For Loops */
+
+/* for(let i = 1; i <= 10; i++) {
+  console.log("Level 1", i);
+  for(let j = 10; j >= 0 ; j -= 2) {
+    console.log(' Level 2', j);
+  }
+
+}; */
+
+const gameBoard = [
+  [4, 32, 8, 4],
+  [64, 8, 32, 2],
+  [8, 32, 16, 4],
+  [2, 8, 4, 2],
+];
+
+let totalScore = 0;
+for(let i = 0; i < gameBoard.length; i++){
+    let row = gameBoard[i];
+    for(let j = 0; j < row.length; j++) {
+     totalScore += row[j];
+    }
+}
