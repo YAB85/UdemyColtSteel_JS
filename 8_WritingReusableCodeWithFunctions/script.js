d326ced7a267f0c22ed2a1051fffd9ab4f0ba63b
Yaroslav <boyaal24@gmail.com>
2020-05-12 21:27:35 +0300

    add exercise with function + loop

--- 8_WritingReusableCodeWithFunctions/script.js
+++ 8_WritingReusableCodeWithFunctions/script.js
@@ -76,11 +76,148 @@ function throwDice (numRolls) {
 
 /*********Functions with Multiple Args */
 
-function square (num) {
+/* function square (num) {
     console.log(num * num);
 }
 
 function sum (x, y) {
     console.log(x + y);
+} */
+
+/*****The return Statement */
+
+/* function add(x, y) {
+    return x + y;
+} */
+
+
+/* function isPurple(color) {
+    if (color.toLowerCase() === 'purple'){
+        return true;
+    } else {
+        return false;
+    }
+} */
+
+
+/* function isPurple(color) {
+    return (color.toLowerCase() === 'purple');
 }
 
+function containsPurple(arr) {
+    for (let color of arr) {
+        if (color === 'purple' || color === 'magenta') {
+            return true;
+        }
+    }
+    return false;
+} */
+
+/******Example use function */
+
+/* function isValidPassword(password, username){
+    if ((password.length < 8) || (password.indexOf(' ') !== -1) || (password.indexOf(username) !== -1) ) {
+        return false;
+    }
+    return true;
+}
+ */
+
+/****** 1 */
+/* function isValidPassword(password, username){
+    if(password.length < 8) {
+        return false;
+    }
+    if(password.indexOf(' ') !== -1 ){ 
+        return false;
+    }
+    if(password.indexOf(username) !== -1) {
+        return false;
+    }
+    return true;
+}
+
+console.log(isValidPassword ('ssdadsad', 'Jack'));
+
+/*******  2 */
+
+/* function isValidPassword(password, username){
+    const tooShort = password.length < 8;
+    const hasSpace = password.indexOf(' ') !== -1;
+    const tooSimilar = password.indexOf(username) !== -1;
+       if(tooShort || hasSpace || tooSimilar) return false;
+    return true;
+} */
+
+/******** 3 */
+
+/* function isValidPassword(password, username){
+    const tooShort = password.length < 8;
+    const hasSpace = password.indexOf(' ') !== -1;
+    const tooSimilar = password.indexOf(username) !== -1;
+       if(!tooShort && !hasSpace && !tooSimilar) return true;
+    return false;
+} */
+
+/********* 4  */
+/* function isValidPassword(password, username){
+    const tooShort = password.length < 8;
+    const hasSpace = password.indexOf(' ') !== -1;
+    const tooSimilar = password.indexOf(username) !== -1;
+    return !tooShort && !hasSpace && !tooSimilar;
+}
+ */
+/******** 5 */
+
+/* function isValidPassword(password, username){
+    return !(password.length < 8) && !(password.indexOf(' ') !== -1) && !(password.indexOf(username) !== -1);
+}
+
+
+
+console.log(isValidPassword ('ss d', 'Jack')); */
+
+
+/******Exercise 2 */
+
+/*******
+ * Write a function to find the avarage value in an array of numbers
+ * avg([0, 50]) //25
+ * avg([75, 76, 80, 95, 100]) // 85.2
+ */
+
+let app = [0, 50, 60];
+let avgSum = [75, 76, 80, 95, 100];
+
+
+function avg(arr) {
+    let total = 0;
+    for (let i = 0; i < arr.length; i++){
+        total += arr[i];
+        
+    }
+    
+    return total / arr.length;;
+    
+}
+
+console.log(avg(app));
+console.log(avg(avgSum));
+
+/* 
+console.log(avg(avgSum)); */
+
+function avg(arr) {
+    let total = 0;
+    //loop over each num
+    for (let num of arr) {
+        //add them together
+        total += num;
+    }
+    //divide by number of nums
+    return total / arr.length;
+}
+
+let arr1 = [75, 76, 80, 95, 100];
+
+console.log(avg(arr1));
