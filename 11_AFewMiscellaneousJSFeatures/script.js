50c0dedf1ae45f6a9e5015d74fb5e732c7f37bcf
Yaroslav <boyaal24@gmail.com>
2020-05-20 22:00:39 +0300

    finished task 11 about rest and spread

--- 11_AFewMiscellaneousJSFeatures/script.js
+++ 11_AFewMiscellaneousJSFeatures/script.js
@@ -1,9 +1,9 @@
-function multiply(x, y) {
+/* function multiply(x, y) {
 	/* if (typeof y === 'undefined') {
         y = 1;
-    } */
+    }
 	return x * y;
-}
+} */
 
 /*function multiply(x, y) {
     y = typeof y === 'undefined' ? 1 : y
@@ -56,7 +56,7 @@ const words = 'abcdefh';
 //const newWords = words.split('');
 const newWords = [ ...words ]; */
 
-const feline = {
+/* const feline = {
 	legs: 4,
 	family: 'Felidae'
 };
@@ -85,12 +85,87 @@ const catDog = {
 
 const catDogClone = {
 	...catDog
-};
+}; */
 
 /******Arguments */
 
-function sum() {
-	return arguments.reduce((total, currVal) => {
+/* function sum() {
+	const argsArr = [ ...arguments ];
+	return argsArr.reduce((total, currVal) => {
 		return total + currVal;
 	});
+} */
+
+/**********Rest */
+
+/* function sum(...nums) {
+	return nums.reduce((total, currVal) => {
+		return total + currVal;
+	});
+}
+
+function fullName(first, last, ...titles) {
+	console.log('first', first);
+	console.log('last', last);
+	console.log('titles', titles);
+} */
+
+/*****Destructuring */
+
+/* const raceResult = [
+	'Eliud Kipchoge',
+	'Feyise Lelisa',
+	'Galen Rupp',
+	'Ghirmay Ghebreslassie',
+	'Alphonce Simbu',
+	'Jared Ward'
+]; */
+
+//to create new value with first three elements in the array
+
+/* const gold = raceResult[0];
+const silver = raceResult[1];
+const bronze = raceResult[2]; */
+
+/* const [ gold, silver, bronze ] = raceResult;
+const [ first, , , , fourth ] = raceResult; */
+
+const runner = {
+	first: 'Eliud',
+	last: 'Kipchoge',
+	country: 'Kenya',
+	title: 'Elder of the Order of the Golden Heart of Kenya'
+};
+
+//const { first, last, time } = runner;
+//const { country: nation, title: honorific } = runner;
+//const { first, last, ...other } = runner;
+
+/* const results = [
+	{
+		first: 'Eliud',
+		last: 'Kipchoge',
+		country: 'Kenya'
+	},
+	{
+		first: 'Feyisa',
+		last: 'Liloka',
+		country: 'Poland'
+	},
+	{
+		first: 'Max',
+		last: 'Pain',
+		country: 'USA'
+	}
+]; */
+
+//const [ , { country } ] = results;
+//const [ { first: goldWinner }, { country } ] = results;
+
+/* function print(person) {
+	const { first, last, title } = person;
+	console.log(`${first}, ${last}, ${title}`);
+} */
+function print({ first, last, title }) {
+	console.log(`${first}, ${last}, ${title}`);
 }
